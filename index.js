b1d56b7a58408f2eba87d1b052c3e77c94dc24cc
Tameka Walls <tamekalwalls@gmail.com>
2024-11-17 10:47:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// I will create a folder entitled JavaScript.
+// I will create a file called indexed.html.
+// I will create a file called index.js.
+// Create peudocode of how to solve this exercise
+// I am building a vault that requires three mathematical 
+// calculations to generate three codes in a combination.
+// Within the HTML, create three variables.
+// The combination of the lock is 10-40-39.
+
+alert("You have received this message because you have been chosen to \n open an important vault.  Here is the secret combination:")
+
+let firstCode = 5 * 2;
+let secondCode = 30 + 10;
+let thirdCode = 49 - 10;
+
+let combination = `${firstCode} - ${secondCode} - ${thirdCode}`
+
+alert(combination)
+
